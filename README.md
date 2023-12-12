8047b0ab9f3af978d0caf2939a2ed18d2188a8f6
theomeuwissen <theo.meuwissen@nmbu.no>
2023-12-12 12:27:20 +0100

    gwablup_wgts.jl updated

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Julia script to calculate GWABLUP weights, that can be used in gghatvr4 to perfo
 
 Example usage:   
 julia gwablup_wgts.jl gwas.lrt  0.001  5   
-gwas.lrt = input file with a single column of log-likelood-ratio-values from GWAS (SNPs are in map order; 1 value per SNP)   
+gwas.lrt = input GWAS results file: 2 columns: SNPeffect & stand.error (SNPs are in map order)   
 0.001 = PI-value to be used   
 5 = number of SNPs used in moving average   
 output file = gwablupwgts.out (2 columns : SNPweight & smoothedLRTvalue)
--- gwablup_wgts.jl
+++ gwablup_wgts.jl
@@ -1,23 +1,24 @@
 # example of usage: 
-# julia gwablup_wgts.jl gwas.lrt  0.01  5
-# gwas.lrt = input file with a single column of log-likelood-ratio-values from GWAS (SNPs are in map order; 1 value per SNP)
-# 0.01 = PI-value to be used
+# julia gwablup_wgts.jl gwas.lrt  0.001  5
+# gwas.bhat.se = input GWAS results file: 2 columns: SNPeffect & stand.error (SNPs are in map order)
+# 0.001 = PI-value to be used
 # 5 = number of SNPs used 
-# output file = gwablup_wgts.out (2 columns : SNPweight & smoothed_LRT_value) 
+# output file = gwablup_wgts.out (2 columns : SNPweight & smoothed_LR_value) 
 using DelimitedFiles, Statistics
 dat=readdlm(ARGS[1])
+lr=0.5*(dat[:,1]./dat[:,2]).^2
 PI=parse(Float64,ARGS[2])
 Navg=parse(Int,ARGS[3])
 Nleft=round(Int,(Navg-1)/2)
 if(Nleft<0)
     Nleft=0
 end    
-println("no of SNPs ",size(dat,1))
+println("no of SNPs ",size(lr,1))
 println("PI= ",PI)
 println("No SNPs to left (and right) included in smoothed average ",Nleft)        
-avg=zeros(size(dat,1),2);
-for i=1:size(dat,1)
-	   avg[i,2]=mean(dat[max(1,i-Nleft):min(i+Nleft,size(dat,1))])
+avg=zeros(size(lr,1),2);
+for i=1:size(lr,1)
+	   avg[i,2]=mean(lr[max(1,i-Nleft):min(i+Nleft,size(lr,1))])
 	   avg[i,1]=PI*exp(avg[i,2])/(PI*exp(avg[i,2])+1-PI)
 end
 writedlm("gwablup_wgts.out",avg)
